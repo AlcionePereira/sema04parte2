dee8d6979c9400f03a2c7978b044c0d67124773d
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-22 20:24:48 -0300

    Add files via upload

---
+++ calcularabc.py
@@ -0,0 +1,18 @@
+def calcular (a, b, c):
+    resultado = 2 * a + 5 * b - c
+    return resultado
+
+a = int(input().strip())
+b = int(input().strip())
+c = int(input().strip())
+
+print(2 * a + 5 * b - c)
+
+
+
+
+
+
+
+
+
