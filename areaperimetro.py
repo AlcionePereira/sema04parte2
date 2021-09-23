51f0b8f97d73066a1c7d89baf2a6ba8c4490c8ba
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-23 17:00:09 -0300

    Add files via upload

---
+++ areaperimetro.py
@@ -0,0 +1,12 @@
+lado_do_quadrado = float(input().strip())
+area_do_quadrado = lado_do_quadrado ** 2
+perimetro = lado_do_quadrado * 4
+print(f'{area_do_quadrado:10.4f}')
+print(f'{perimetro:10.4f}')
+
+
+
+      
+
+
+
